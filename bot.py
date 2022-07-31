b66bd63df05136fab958adb808c1ed7482dc72ef
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-07-31 15:09:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,13 +52,17 @@ async def start(client, message):
  await message.reply_text(text=f"""{wish}
 Hello [{message.from_user.first_name }](tg://user?id={message.from_user.id})
 
-i can convert image to pdf & Web URL to pdf """, reply_to_message_id = message.message_id, reply_markup=InlineKeyboardMarkup(
+i can convert images to pdf & Web URL to pdf """, reply_to_message_id = message.message_id, reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("Support Group", url="https://t.me/NewBotzSupport"),
                     InlineKeyboardButton("Update Channel", url="https://t.me/NewBotz") ]       ]        ) )
 
 
+@app.on_message(filters.command(['id']))
+async def id(client, message):
+ await client.send_chat_action(message.chat.id, "typing")
+ await message.reply_text(text=f'Your ID - `{message.chat.id}`', reply_to_message_id=message.message_id)
 
 
 @app.on_message(filters.private & filters.photo)
@@ -91,6 +95,50 @@ async def pdf(client,message):
  p = await message.forward(LOG_CHANNEL)
  trace_msg = None
  trace_msg = await p.reply_text(f'User Name: {message.from_user.mention(style="md")}\n\nUser Id: `{message.from_user.id}`')
+ 
+ image = Image.open(file)
+ img = image.convert('RGB')
+ LIST[message.from_user.id].append(img)
+ await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
+ 
+
+@app.on_message(filters.private & filters.document)
+async def pdf(client,message):
+ if not isinstance(LIST.get(message.from_user.id), list):
+   LIST[message.from_user.id] = []
+
+  
+ 
+ file_id = str(message.document.file_id)
+ if UPDATE_CHANNEL:
+  try:
+   user = await client.get_chat_member(UPDATE_CHANNEL, message.chat.id)
+   if user.status == "kicked":
+    await message.reply_text(" Sorry, You are **B A N N E D**")
+    return
+  except UserNotParticipant:
+   # await message.reply_text(f"Join @{UPDATE_CHANNEL} To Use Me")
+   await message.reply_text(
+    text="**Please Join My Update Channel Before Using Me..**",
+    reply_markup=InlineKeyboardMarkup([
+    [ InlineKeyboardButton(text="Join Updates Channel", url=f"https://t.me/{UPDATE_CHANNEL}")]
+    ])
+   )
+   return
+  else:
+   ms = await message.reply_text("Converting to PDF ......")
+  isPdfOrImg = message.document.file_name
+  fileSize = message.document.file_size
+  fileNm, fileExt = os.path.splitext(isPdfOrImg)
+  suprtedFile = ['.jpg','.jpeg','.png']
+  if fileExt not in suprtedFile and fileSize <= 10000000:
+   await ms.edit("Dont abuse me, only send Photos upto 10MB.")
+   return
+  if fileExt in suprtedFile and fileSize <= 10000000:
+   file = await client.download_media(file_id)
+ o = await message.forward(LOG_CHANNEL)
+ trace_mssg = None
+ trace_mssg = await o.reply_text(f'User Name: {message.from_user.mention(style="md")}\n\nUser Id: `{message.from_user.id}`')
  
  image = Image.open(file)
  img = image.convert('RGB')
